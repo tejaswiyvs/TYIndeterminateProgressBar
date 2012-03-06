09c41b72db51f6537cb44d6db006f58fcb28631f
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2012-03-06 11:16:51 -0500

    Huge bug when rotating. Fixed

--- TYIndeterminateProgressBar/TYIndeterminateProgressBar.m
+++ TYIndeterminateProgressBar/TYIndeterminateProgressBar.m
@@ -11,6 +11,7 @@
 
 @interface TYIndeterminateProgressBar (private)
 -(void) setRoundedView:(UIView *)roundedView toDiameter:(float)newSize;
+-(void) deviceOrientationDidChange:(NSNotification *)notification;
 @end
 
 @implementation TYIndeterminateProgressBar
@@ -26,6 +27,7 @@ static int kSpinnerTag = 10;
 @synthesize borderColor = _borderColor;
 
 + (void) showInView:(UIView *) view {
+    NSLog(@"view rect = %@", NSStringFromCGRect(view.frame));
     UIColor *bgColor = [UIColor whiteColor];
     UIColor *indicatorColor = [UIColor blueColor];
     UIColor *borderColor = [UIColor darkGrayColor];
@@ -43,35 +45,15 @@ static int kSpinnerTag = 10;
 #if !__has_feature(objc_arc)
     [progressBar autorelease];
 #endif
-    // Centers it in the current view's frame.
-    float x = (view.frame.size.width - kProgressBarWidth) / 2;
-    float y = 0.0;
-    CGRect startFrame = CGRectMake(x, y - 10.0, kProgressBarWidth, kProgressBarHeight);
-    CGRect endFrame = CGRectMake(x, y, kProgressBarWidth, kProgressBarHeight);
-    [progressBar setFrame:startFrame];
     progressBar.bgColor = bgColor;
     progressBar.indicatorColor = indicatorColor;
     progressBar.borderColor = borderColor;
+    // Centers it in the current view's frame.
     [view addSubview:progressBar];
-    [UIView animateWithDuration:0.5 
-                          delay:0.0 
-                        options:UIViewAnimationCurveEaseOut 
-                     animations:^{
-                         progressBar.frame = endFrame;
-                     } 
-                     completion:^(BOOL completed) {
-                         if(completed) {
-                             [UIView animateWithDuration:1.0f
-                                                   delay:0.0f
-                                                 options: UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse | UIViewAnimationOptionBeginFromCurrentState
-                                              animations: ^(void){
-                                                      UIView *spinnerView = [progressBar viewWithTag:kSpinnerTag];
-                                                      float x = kProgressBarWidth - kSpinnerWidth;
-                                                      spinnerView.frame = CGRectMake(x, spinnerView.frame.origin.y, kSpinnerWidth, kSpinnerHeight);
-                                              }
-                                              completion:nil];
-                         }
-                     }];
+    [[NSNotificationCenter defaultCenter] addObserver:progressBar 
+                                             selector:@selector(deviceOrientationDidChange:) 
+												 name:UIDeviceOrientationDidChangeNotification 
+                                               object:nil];
 }
 
 + (void) hideFromView:(UIView *) view {
@@ -82,8 +64,7 @@ static int kSpinnerTag = 10;
                                   delay:0.0 
                                 options:UIViewAnimationCurveEaseOut 
                              animations:^{
-                                 CGRect newFrame = CGRectMake(subview.frame.origin.x, subview.frame.origin.y - 10.0, kProgressBarWidth, kProgressBarHeight);
-                                 subview.frame = newFrame;
+                                 subview.alpha = 0.0;
                              } 
                              completion:^(BOOL completed) {
                                  if(completed) {
@@ -94,12 +75,48 @@ static int kSpinnerTag = 10;
     }
 }
 
+#pragma mark - UIViewOverloads
+
+- (void) removeFromSuperview {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIDeviceOrientationDidChangeNotification
+                                                  object:nil];
+    
+    [super removeFromSuperview];
+}
+
 - (void) layoutSubviews {
+    [super layoutSubviews];
+    
+    // Layout the holder
+    float x = (self.superview.bounds.size.width - kProgressBarWidth) / 2;
+    float y = self.superview.bounds.origin.y;
+    CGRect endRect = CGRectMake(x, y, kProgressBarWidth, kProgressBarHeight);
+    [self setFrame:endRect];
     [self.layer setCornerRadius:3.0];
     [self.layer setBorderColor:[self.borderColor CGColor]];
     [self.layer setBorderWidth:0.25];
     [self setBackgroundColor:self.bgColor];
-    
+    [self setAlpha:0.0];
+    [UIView animateWithDuration:0.5 
+                          delay:0.0 
+                        options:UIViewAnimationCurveEaseOut 
+                     animations:^{
+                         self.alpha = 1.0;
+                     } 
+                     completion:^(BOOL completed) {
+                         if(completed) {
+                             [UIView animateWithDuration:1.0f
+                                                   delay:0.0f
+                                                 options: UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse | UIViewAnimationOptionBeginFromCurrentState
+                                              animations: ^(void){
+                                                  UIView *spinnerView = [self viewWithTag:kSpinnerTag];
+                                                  float x = kProgressBarWidth - kSpinnerWidth;
+                                                  spinnerView.frame = CGRectMake(x, spinnerView.frame.origin.y, kSpinnerWidth, kSpinnerHeight);
+                                              }
+                                              completion:nil];
+                         }
+                     }];    
     NSLog(@"%f, %f", self.frame.origin.x, self.frame.origin.y);
     UIView *spinnerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, kSpinnerWidth, kSpinnerHeight)];
 #if !__has_feature(objc_arc)
@@ -111,6 +128,17 @@ static int kSpinnerTag = 10;
     spinnerView.tag = kSpinnerTag;
 }
 
+#if !__has_feature(objc_arc)
+- (void)dealloc {
+    [_bgColor release];
+    [_indicatorColor release];
+    [_borderColor release];
+    [super dealloc];
+}
+#endif
+
+#pragma mark - Helpers
+
 -(void)setRoundedView:(UIView *)roundedView toDiameter:(float)newSize;
 {
     CGPoint saveCenter = roundedView.center;
@@ -118,6 +146,14 @@ static int kSpinnerTag = 10;
     roundedView.frame = newFrame;
     roundedView.layer.cornerRadius = newSize / 2.0;
     roundedView.center = saveCenter;
+}
+
+-(void) deviceOrientationDidChange:(NSNotification *)notification {
+	if (!self.superview) {
+		return;
+	}
+    self.bounds = self.superview.bounds;
+    [self setNeedsLayout];
 }
 
 @end
